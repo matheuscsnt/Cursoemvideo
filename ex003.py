2feae70061b4c76cf2d356e3268e94617e9b4d13
matheuscsnt <santossmathheus@gmail.com>
2023-12-06 19:36:27 -0300

    Exercício 003 precisa de uma revisão.

---
+++ ex003.py
@@ -0,0 +1,7 @@
+numero1 = input('Digite um número: ')
+numero2 = input('Digite um segundo número: ')
+resultado = numero1 + numero2
+
+print('Essa é a soma dos dois números: ', resultado)
+
+# esse exercício está incorreto, a ideia era buscar o resultado e apresentar nesse print, porém ele está como uma string
