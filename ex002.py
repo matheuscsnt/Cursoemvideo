9f47eca35f9541c6f0e349e0e8e01c8be13ced2c
matheuscsnt <santossmathheus@gmail.com>
2023-12-06 19:22:36 -0300

    Mais um exercício em python do Curso em video.

---
+++ ex002.py
@@ -0,0 +1,5 @@
+data = input('Qual a data do seu nascimento? ')
+mes = input('Qual o mês do seu nascimento? ')
+ano = input('Qual o ano do seu nascimento? ')
+
+print('Você nasceu em:', data, '/', mes, '/', ano)
