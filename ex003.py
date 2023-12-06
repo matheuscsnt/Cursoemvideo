2011920a117104de0f7944187e91db6de88821b6
matheuscsnt <santossmathheus@gmail.com>
2023-12-06 19:46:35 -0300

    Exercício 003 precisa de uma revisão.

--- ex003.py
+++ ex003.py
@@ -5,3 +5,4 @@ resultado = numero1 + numero2
 print('Essa é a soma dos dois números: ', resultado)
 
 # esse exercício está incorreto, a ideia era buscar o resultado e apresentar nesse print, porém ele está como uma string
+
