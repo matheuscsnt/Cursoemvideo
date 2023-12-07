a96f505c06e3c10b4826945a2f1849cc59b1de4d
matheuscsnt <santossmathheus@gmail.com>
2023-12-07 12:34:54 -0300

    Exercício corrigido após a aula de tipos primitivos.

--- ex003.py
+++ ex003.py
@@ -1,7 +1,9 @@
-numero1 = input('Digite um número: ')
-numero2 = input('Digite um segundo número: ')
+numero1 = int(input('Digite um número: '))
+numero2 = int(input('Digite um segundo número: '))
 resultado = numero1 + numero2
 
 print('Essa é a soma dos dois números: ', resultado)
 
 # esse exercício está incorreto, a ideia era buscar o resultado e apresentar nesse print, porém ele está como uma string
+
+# falha corrigida, aprendi na aula de tipos primitivos e saída de dados que o input trata as coisas como string, precisamos definir se quisermos algo diferente disso.
