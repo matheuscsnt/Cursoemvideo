5fee018aa3019c2c41512106ddc8228efcd350a1
matheuscsnt <santossmathheus@gmail.com>
2023-12-13 12:54:46 -0300

    Transformando metro em centímetro e milímetro.

---
+++ ex009.py
@@ -0,0 +1,6 @@
+metragem = float(input('Digite quantos metros possui o ambiente: '))
+
+centimetros = metragem * 100
+milimetros = metragem * 1000
+
+print(f'O ambiente tem: {metragem} metros, transformando para centimetros ele possui: {centimetros} centimentros e em milimetros são: {milimetros} milimetros.')
