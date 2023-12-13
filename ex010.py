1802a6f3ed96ef0928421c0e00e798e025dd4d72
matheuscsnt <santossmathheus@gmail.com>
2023-12-13 12:59:42 -0300

    Recebendo um número e fazendo a sua tabuada.

---
+++ ex010.py
@@ -0,0 +1,25 @@
+numero = int(input('Digite um numero: '))
+
+num_tabuada1 = numero * 1
+num_tabuada2 = numero * 2
+num_tabuada3 = numero * 3
+num_tabuada4 = numero * 4
+num_tabuada5 = numero * 5
+num_tabuada6 = numero * 6
+num_tabuada7 = numero * 7
+num_tabuada8 = numero * 8
+num_tabuada9 = numero * 9
+num_tabuada10 = numero * 10
+
+print(f''' A tabuada deste numero é:
+    {numero}x1 = {num_tabuada1},
+    {numero}x2 = {num_tabuada2},
+    {numero}x3 = {num_tabuada3},
+    {numero}x4 = {num_tabuada4},
+    {numero}x5 = {num_tabuada5},
+    {numero}x6 = {num_tabuada6},
+    {numero}x7 = {num_tabuada7},
+    {numero}x8 = {num_tabuada8},
+    {numero}x9 = {num_tabuada9},
+    {numero}x10 = {num_tabuada10}.
+      ''')
