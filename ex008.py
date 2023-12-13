6125acc439ed63202f724937c65112f789385101
matheuscsnt <santossmathheus@gmail.com>
2023-12-13 12:40:53 -0300

    Recebendo as notas de um aluno e mostrando a sua média.

---
+++ ex008.py
@@ -0,0 +1,8 @@
+aluno = (input('Insira o nome do(a) aluno(a): '))
+
+nota1 = float(input('Insira a primeira nota do aluno: '))
+nota2 = float(input('Insira a segunda nota do aluno: '))
+
+media = (nota1 + nota2) / 2
+
+print(f'A média de {aluno}, foi {media}.')
