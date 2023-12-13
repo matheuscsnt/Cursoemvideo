0ae41234ceb642973bbacfe6aa372e29992692cd
matheuscsnt <santossmathheus@gmail.com>
2023-12-13 12:23:57 -0300

    Recebendo um número e mostrandos seu dobro, triplo e raiz quadrada.

---
+++ ex007.py
@@ -0,0 +1,7 @@
+numero = int(input('Digite um número: '))
+
+dobro_num = numero * 2
+triplo_num = numero * 3
+raiz_num = numero ** (1/2)
+
+print(f'Este foi o número digitado: {numero}, o dobro desse número é: {dobro_num}, o triplo desse número é: {triplo_num} e essa é a raiz quadrada dele: {raiz_num}.')
