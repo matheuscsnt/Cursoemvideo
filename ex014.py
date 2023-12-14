9dde35dcd5d6ce5044587bdef2a1fe8ff49be9c4
matheuscsnt <santossmathheus@gmail.com>
2023-12-14 13:36:59 -0300

    Exercício de aumento de salário em 15%.

---
+++ ex014.py
@@ -0,0 +1,7 @@
+salario = float(input('Digite o seu salário: '))
+
+aumento = salario * 0.15
+
+novo_salario = salario + aumento
+
+print (f'Com o aumento de 15%, o seu novo salário será R${novo_salario:.2f}.')
