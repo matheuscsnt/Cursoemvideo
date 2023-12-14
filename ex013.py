f59fb63a0f1e415273ca46d3e3531ac388d2676b
matheuscsnt <santossmathheus@gmail.com>
2023-12-14 13:33:06 -0300

    Lendo o valor de um produto e aplicando desconto de 5%.

---
+++ ex013.py
@@ -0,0 +1,7 @@
+preco_produto = float(input('Qual o preço do produto: '))
+
+desconto = preco_produto * 0.05
+
+produto_desconto = preco_produto - desconto
+
+print (f'Você ganhou um desconto de 5%! O produto sairá por apenas R${produto_desconto:.2f}.')
