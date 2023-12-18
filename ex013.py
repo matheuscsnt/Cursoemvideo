b07528c0daea2a5ee5babf5af251be537b0e0170
matheuscsnt <santossmathheus@gmail.com>
2023-12-18 20:15:59 -0300

    Update no exercício de desconto.

--- ex013.py
+++ ex013.py
@@ -4,4 +4,4 @@ desconto = preco_produto * 0.05
 
 produto_desconto = preco_produto - desconto
 
-print (f'Você ganhou um desconto de 5%! O produto sairá por apenas R${produto_desconto:.2f}.')
+print(f'Você ganhou um desconto de 5%! O produto sairá por apenas R${produto_desconto:.2f}.')
