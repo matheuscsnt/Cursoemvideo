d04e10d5249300f6cab2086963240ab9e75cfc99
matheuscsnt <santossmathheus@gmail.com>
2023-12-20 22:33:25 -0300

    Programa para reconhecer velocidade e aplicar multa se necessário.

--- ex031.py
+++ ex031.py
@@ -4,4 +4,5 @@ if velocidade > 80.0:
     print(f'Você foi multado, pois estava a {velocidade}Km/h.')
 else:
     print(f'Velocidade dentro do permitido. Passou a {velocidade}Km/h.')
+
 # A multa vai custar 7$ por km/h a mais. Adicionarei essa parte no momento da correção.
