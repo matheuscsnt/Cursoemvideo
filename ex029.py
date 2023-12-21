ffdc0ad62d11e24df9547f167a0c07a556fdd319
matheuscsnt <santossmathheus@gmail.com>
2023-12-20 21:40:25 -0300

    Iniciando as aulas de condições.

---
+++ ex029.py
@@ -0,0 +1,5 @@
+tempo = int(input('Quantos anos tem seu carro? '))
+if tempo <= 3:
+    print(f'O seu carro é novo, possui apenas {tempo} ano(s) de uso.')
+else:
+    print(f'O seu carro está velho, ele possui {tempo} ano(s) de uso.')
