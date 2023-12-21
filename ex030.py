b3faccff31982b82237cec78eb3f87061cc8ddee
matheuscsnt <santossmathheus@gmail.com>
2023-12-20 22:50:06 -0300

    Jogo de adivinhação v1.0

--- ex030.py
+++ ex030.py
@@ -1,14 +1,12 @@
-from random import choice
-n0 = 0
-n1 = 1
-n2 = 2
-n3 = 3
-n4 = 4
-n5 = 5
-lista = [n0, n1, n2, n3, n4,n5]
+from random import randint
+from time import sleep
+print('Vou pensar em um número entre 0 e 5. Tente adivinhar...')
 
-escolhido = choice(lista)
-num_escolhido = int(input('Tende adivinhar qual foi o número escolhido de 0 a 5: '))
+escolhido = randint(0,5)
+# RANDINT sorteia um número. A principio tinha feito com choice.
+num_escolhido = int(input('Em que número eu pensei? '))
+print('Processando...')
+sleep(2)
 if num_escolhido == escolhido:
     print(f'Parabéns, você acertou! O número escolhido pelo PC foi {escolhido}!')
 else:
