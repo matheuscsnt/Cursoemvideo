0098b079a42716b70e2864bcbc75c2369dad9cbe
matheuscsnt <santossmathheus@gmail.com>
2023-12-22 15:30:25 -0300

    Correção do radar eletrônico.

--- ex031.py
+++ ex031.py
@@ -2,6 +2,8 @@ velocidade = float(input('Qual a velocidade do veículo? '))
 
 if velocidade > 80.0:
     print(f'Você foi multado, pois estava a {velocidade}Km/h.')
-else:
-    print(f'Velocidade dentro do permitido. Passou a {velocidade}Km/h.')
+    multa = (velocidade-80) * 7
+    print(f'Você deve pagar uma multa no valor de R${multa:.2f}.')
+print(f'Velocidade dentro do permitido. Passou a {velocidade}Km/h.')
 # A multa vai custar 7$ por km/h a mais. Adicionarei essa parte no momento da correção.
+# Testando a condição simples, nao utilizando o else.
