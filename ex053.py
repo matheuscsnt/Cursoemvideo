7a697361faedc3d67f6d60ffe1460ca38950ced9
matheuscsnt <santossmathheus@gmail.com>
2024-01-04 20:27:05 -0300

    Exercício de progressão aritmética.

---
+++ ex053.py
@@ -0,0 +1,8 @@
+from time import sleep
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razão: '))
+decimo = primeiro + (10 - 1) * razao
+for c in range(primeiro, decimo + razao, razao):
+    print(f'{c}', end=' > ')
+    sleep(0.3)
+print('FINISH!')
