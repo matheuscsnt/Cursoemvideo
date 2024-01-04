77b341a4f0de8415698acf27c08eec4bfefa345b
matheuscsnt <santossmathheus@gmail.com>
2024-01-04 13:17:09 -0300

    Contagem regressiva usando laço de repetição. Corrigido.

--- ex048.py
+++ ex048.py
@@ -1,7 +1,7 @@
 from time import sleep
 print('Seja bem vindo a contagem regressiva!')
 
-for contagem in range(0, 11):
+for contagem in range(10, -1, -1):
     print(contagem)
     sleep(1)
 print('FIM.')
