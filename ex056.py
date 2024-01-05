24e93c40829df21941a806d370a05a27772eb3f2
matheuscsnt <santossmathheus@gmail.com>
2024-01-04 21:19:09 -0300

    Identificando quantidade de pessoas maiores de idade.

--- ex056.py
+++ ex056.py
@@ -3,7 +3,7 @@ atual = date.today().year
 totalmaior = 0
 totalmenor = 0
 for pess in range(1, 8):
-    nasc = int(input(f'Em que ano a {pess}º pessoa nasceu? '))
+    nasc = int(input(f'Em que ano a {pess}ª pessoa nasceu? '))
     idade = atual - nasc
     if idade >= 21:
         totalmaior += 1
